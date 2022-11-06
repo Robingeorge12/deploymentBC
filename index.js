95b92fe7e752c2199013008118d59b159163599a
Robingeorge12 <robingsa.btch@gmail.com>
2022-11-06 14:40:14 +0530

    BC latest

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
-//require('./deploymentBackend/node_modules/dotenv/lib/main').config()
-const PORT = process.env.env || 7003
+require('dotenv').config()
+const PORT = process.env.PORT || 7003
 
 app.get("/",(req,res)=>{
     res.send("welcome")
